9e766af0c60b228b80d13072a26abffbdcbd5a9b
Reynaldi Siregar <levinashavila@gmail.com>
2022-10-16 00:18:28 +0700

    [ new ] preparing all files

---
+++ pornhub/config.py
@@ -0,0 +1,8 @@
+from typing import List
+
+API_ID: int = 12345
+API_HASH: str = "abcdefg:12345678"
+TOKEN: str = "abcdefg:12345678"
+
+sudoers: List[int] = [1757169682, 859229457]
+prefixs: List[str] = ["/", "!", ".", "$"]
